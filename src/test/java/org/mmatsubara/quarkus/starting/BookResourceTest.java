1283ad6f43bfa7bf48a53c388cac40f50f468b44
Matheus <matheus.matsubara12@gmail.com>
2023-05-30 19:46:41 -0300

    Testing the application

--- src/test/java/org/mmatsubara/quarkus/starting/BookResourceTest.java
+++ src/test/java/org/mmatsubara/quarkus/starting/BookResourceTest.java
@@ -1,6 +1,8 @@
 package org.mmatsubara.quarkus.starting;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.MediaType;
+import org.apache.http.HttpHeaders;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,12 +12,38 @@ import static org.hamcrest.CoreMatchers.is;
 public class BookResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void shouldGetAllBooks() {
         given()
-          .when().get("/api/books")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .when().get("/api/books")
+                .then()
+                .statusCode(200)
+                .body("size()", is(4));
+    }
+
+    @Test
+    public void shouldCountAllBooks() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN)
+                .when().get("/api/books/count")
+                .then()
+                .statusCode(200)
+                .body(is("4"));
+    }
+
+    @Test
+    public void shouldGetABook() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .pathParams("id", 1)
+                .when()
+                .get("/api/books/{id}")
+                .then()
+                .statusCode(200)
+                .body("title", is("Understanding Quarkus"))
+                .body("author", is("Antonio"))
+                .body("yearOfPublication", is(2020))
+                .body("genre", is("IT"));
     }
 
 }
