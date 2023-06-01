37b85b5c8c39a2b209cc2c989dadaee31dd5bfd3
Matheus <matheus.matsubara12@gmail.com>
2023-06-01 17:31:00 -0300

    Packaging the Application

--- src/test/java/org/mmatsubara/quarkus/starting/BookResourceIT.java
+++ src/test/java/org/mmatsubara/quarkus/starting/BookResourceIT.java
@@ -1,8 +1,28 @@
 package org.mmatsubara.quarkus.starting;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import jakarta.ws.rs.core.MediaType;
+import org.apache.http.HttpHeaders;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusIntegrationTest
 public class BookResourceIT extends BookResourceTest {
-    // Execute the same tests but in packaged mode.
+
+    @Test
+    public void shouldGetABook() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .pathParams("id", 1)
+                .when()
+                .get("/api/books/{id}")
+                .then()
+                .statusCode(200)
+                .body("title", is("Understanding Quarkus"))
+                .body("author", is("Antonio"))
+                .body("yearOfPublication", is(2020))
+                .body("genre", is("Information Technology"));
+    }
 }
